57b454194b3997289bd91d71378fc770c907dbc8
daniele-atzeni <33978400+daniele-atzeni@users.noreply.github.com>
2018-12-19 00:42:50 +0100

    prova dataset

--- NN.py
+++ NN.py
@@ -7,6 +7,7 @@ Created on Tue Nov  6 14:20:03 2018
 
 import numpy as np
 from math import e
+from math import sqrt
 from random import uniform
 
 '''
@@ -23,25 +24,33 @@ def sign(x):
 def sigmoid(x):
     return 1 / (1 + e**(-x))
 
+def relu(x):
+    if x < 0:
+        return 0
+    return x
+
 def der(func):
     if func == identity:
         return lambda x: 1
     if func == sigmoid:
         return lambda x: sigmoid(x) * (1 - sigmoid(x))
+    if func == relu:
+        return lambda x: 0 if x <= 0 else 1
 
-def check_zero(matrix_list):
-    for matrix in matrix_list:
-            for row in matrix:
-                for elem in row:
-                    if elem != 0:
-                        return False
-    return True
 
 def my_sum(matrix_list1, matrix_list2):
     return [matrix_list1[i] + matrix_list2[i] for i in range(len(matrix_list1))]
 
 def my_X_scal(scalar, matrix_list):
     return [scalar * matrix for matrix in matrix_list]
+
+def norm(matrix_list):
+    norm = 0
+    for matrix in matrix_list:
+        for row in matrix:
+            for elem in row:
+                norm += elem**2
+    return sqrt(norm)
 
 '''
 Una rete neurale viene rappresentata con una lista di matrici, una per ogni layer diverso
@@ -126,16 +135,16 @@ class NeuralNetwork:
             for j in range(len(self.layers[i])):
                 for k in range(len(self.layers[i][j])):
                     self.layers[i][j][k] = uniform(-0.7, 0.7)
-                    
+
     def predict(self, data):
         output_arr = []
         len_data = len(data)
         for i in range(len_data):
             outputNN = self.forward(data[i])
             output_arr.append([round(elem) for elem in outputNN])
-        
+
         return output_arr
-   
+
     def fit(self, train_data, train_class, toll, learning_rate, MAX_ITER):
         error = float('inf')
         gradient = [np.ones(layer.shape) for layer in self.layers]
@@ -144,35 +153,36 @@ class NeuralNetwork:
 
         # NB: i pesi vanno aggiornati solo quando l'errore è troppo grande,
         # quindi appena entro nel while, non alla fine del while
-        while(error > toll and n_iter < MAX_ITER and not check_zero(gradient)):
+        while(error > toll and n_iter < MAX_ITER and norm(gradient) > 10**(-15) ):
             if n_iter != 0:
                 self.layers = my_sum(self.layers, my_X_scal(learning_rate, gradient))
             first = True
             # calcolo del gradiente e dell'errore
             for index, pattern in enumerate(train_data):
                 outputNN = self.forward(pattern)
-                out_round = [round(el) for el in outputNN]
+                # out_round = [round(el) for el in outputNN]
                 if first:
                     gradient = self.backward(outputNN, train_class[index])
                     error = sum((outputNN - train_class[index]) ** 2)
-                    accuracy_err = sum([abs(el) for el in out_round - train_class[index]])
+                    #accuracy_err = sum([abs(el) for el in out_round - train_class[index]])
                     first = False
                 else:
                     gradient = my_sum(gradient, self.backward(outputNN, train_class[index]))
                     error += sum((outputNN - train_class[index]) ** 2)
-                    accuracy_err += sum([abs(el) for el in out_round-train_class[index]])
+                    #accuracy_err += sum([abs(el) for el in out_round-train_class[index]])
             error = error / len(train_data)
             print(error)
-            print(accuracy_err)
+            print(norm(gradient))
+            #print(accuracy_err)
             n_iter += 1
-            
+
         print(n_iter)
 
         return error
 
 ###################-----------PROVA--------###########################
 
-'''     PROVA BACKWARD     
+'''     PROVA BACKWARD
 NN = NeuralNetwork((2, 2, 2), 2*[sigmoid])
 NN.layers = [np.array([[0.15, 0.25, 0.35], [0.2, 0.3, 0.35]]), np.array([[0.4, 0.5, 0.6], [0.45, 0.55, 0.6]])]
 out = NN.forward(np.array([0.05, 0.1]))
@@ -181,7 +191,7 @@ grad = NN.backward(out, np.array([0.8, 0.7]))
 print(grad)
 '''
 
-''' PROVA 1  ''' 
+''' PROVA MONK
 
 data = np.genfromtxt("Monk1.txt")
 target = [np.array(row[0]).astype('float32') for row in data]
@@ -205,3 +215,16 @@ for i in range(len(prediction)):
         count += 1
 
 print(error_test/len(prediction), count)
+'''
+
+''' PROVA TRAINING SET '''
+data = np.genfromtxt("ML-CUP18-TR.csv", delimiter=',')[:, 1:]
+# normalization of data
+data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
+
+target = [np.array(row[-2:]).astype('float32') for row in data]
+train_set = [np.array(row[:-2]) for row in data]
+
+NN = NeuralNetwork((len(train_set[0]), 500, 500, 2), 3*[relu])
+error = NN.fit(train_set, target, 0.0001, 0.05, 500)
+print('errore = ', error)
