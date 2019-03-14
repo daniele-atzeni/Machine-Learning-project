9daa92781f62352ce739d8c8a4d8e5940ae736d8
daniele-atzeni <33978400+daniele-atzeni@users.noreply.github.com>
2019-03-14 15:43:31 +0100

    dovrebbe andare bene

--- NN.py
+++ NN.py
@@ -209,7 +209,7 @@ class NeuralNetwork:
         # i pesi vanno inizializzati più volte
         # ogni volta che li inizializziamo facciamo ripartire l'algoritmo vero e proprio
         # memorizziamo l'errore minimo di ogni tentativo e i pesi migliori
-        for n in range(self.n_init):
+        for _ in range(self.n_init):
             #print('inizializzazione', n + 1)
             curr_error = float('inf')
             gradient = [np.zeros_like(layer) for layer in self.weights]
@@ -285,7 +285,7 @@ class NeuralNetwork:
         # NB predicted_y è un array, così come data_y
         error_list = [sum((prediction - data_y[index])**2) for index, prediction in enumerate(predicted_y)]
         return sum(error_list) / len(error_list)
-
+'''
     def k_fold_cv(self, data, k=5):
     # ritorna una lista di score (MSE/accuracy), uno per ogni tentativo
         # errore se la rete non è stata fittata --> non si conosce il numero di input e output
@@ -346,10 +346,10 @@ class NeuralNetwork:
             error_list.append(test_error)
         
         return error_list
+'''
 
 
-
-###################-----------PROVA--------###########################
+###################-----------MAIN--------###########################
 '''
 PROVA MONK 1
 
@@ -384,7 +384,7 @@ plt.show()
 '''
 '''
 PROVA MONK 2
-'''
+
 data = np.genfromtxt("monk2.txt")
 train_y = data[:, 0]
 train_y = train_y.reshape((train_y.shape[0], 1))
@@ -425,10 +425,10 @@ for neuron in neurons_per_layer:
                     plt.xlabel('number of epochs')
                     plt.ylabel('accuracy')
                     plt.show()
-
+'''
 '''
 PROVA MONK 3
-
+'''
 data = np.genfromtxt("monk3.txt")
 train_y = data[:, 0]
 train_y = train_y.reshape((train_y.shape[0], 1))
@@ -440,24 +440,42 @@ test_x = data_test[:, 1:-1]
 # normalizzazione min-max
 train_x = ( train_x - np.array([min(train_x[:, i]) for i in range(train_x.shape[1])]) ) / np.array([max(train_x[:, i]) for i in range(train_x.shape[1])])
 test_x = ( test_x - np.array([min(test_x[:, i]) for i in range(test_x.shape[1])]) ) / np.array([max(test_x[:, i]) for i in range(test_x.shape[1])])
-# allenamento
-NN = NeuralNetwork(4 * [10], 4 * ['tanh'], classification=True, learning_rate=0.01, Lambda=0, alpha=0.9, toll=0.000001, n_init=1, max_epochs=200, minibatch_size=2)
-error_list, n_epochs, test_error_list, acc_list, test_acc_list = NN.fit(train_x, train_y, test_x, test_y)
-plt.plot(range(n_epochs + 1), error_list)
-plt.plot(range(n_epochs + 1), test_error_list, ls='dashed')
-plt.legend(['train error', 'test error'])
-plt.title('MSE vs number of epochs')
-plt.xlabel('number of epochs')
-plt.ylabel('MSE')
-plt.show()
-plt.plot(range(n_epochs + 1), acc_list)
-plt.plot(range(n_epochs + 1), test_acc_list, ls='dashed')
-plt.legend(['train accuracy', 'test accuracy'])
-plt.title('accuracy vs number of epochs')
-plt.xlabel('number of epochs')
-plt.ylabel('accuracy')
-plt.show()
-'''
+# grid seach
+learning_rates = np.arange(0.01, 0.1, 0.01)
+lambdas = np.arange(0.1, 1, 0.1)
+alphas = np.arange(0.1, 1, 0.1)
+neurons_per_layer = [5, 10, 20, 30]
+minibatch_sizes = [None, 2, 4, 8, 16, 32]
+layers_numbers = [3, 4]
+for neuron in neurons_per_layer:
+    for layer in layers_numbers:
+        for learning_rate in learning_rates:
+            for Lambda in lambdas:
+                for alpha in alphas:
+                    for  minibatch_size in minibatch_sizes:
+                        titolo = 'layer = ' + str(layer * [neuron]) + ', funzioni = ' + str(layer * ['tanh']) + ', learning_rate = ' + str(learning_rate) + ', Lambda = ' + str(Lambda) + ', alpha = ' + str(alpha) + ', minibatch_size = ' + str(minibatch_size)
+                        print(titolo)
+                        NN = NeuralNetwork(layer * [neuron], layer * ['tanh'], classification=True, learning_rate=learning_rate, Lambda=Lambda, alpha=alpha, toll=0.000001, n_init=1, max_epochs=100, minibatch_size=minibatch_size)
+                        error_list, n_epochs, test_error_list, acc_list, test_acc_list = NN.fit(train_x, train_y, test_x, test_y)
+                        print('train error = ' + str(error_list[-1]), 'test_error = ' + str(test_error_list[-1]), 'train accuracy = ' + str(acc_list[-1]), 'test accuracy = ' + str(test_acc_list[-1]))
+                        if (test_acc_list[-1] >= 0.8):
+                            plt.plot(range(n_epochs + 1), error_list)
+                            plt.plot(range(n_epochs + 1), test_error_list, ls='dashed')
+                            plt.legend(['train error', 'test error'])
+                            plt.title('MSE')
+                            plt.xlabel('number of epochs')
+                            plt.ylabel('MSE')
+                            plt.savefig('MSE ' + titolo + '.png')
+                            plt.close()
+                            plt.plot(range(n_epochs + 1), acc_list)
+                            plt.plot(range(n_epochs + 1), test_acc_list, ls='dashed')
+                            plt.legend(['train accuracy', 'test accuracy'])
+                            plt.title('accuracy')
+                            plt.xlabel('number of epochs')
+                            plt.ylabel('accuracy')
+                            plt.savefig('accuracy ' + titolo + '.png')
+                            plt.close()
+
 ''' 
 PROVA TRAINING SET
 
